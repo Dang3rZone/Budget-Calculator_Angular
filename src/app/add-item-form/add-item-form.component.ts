54c92c69f574d464e2ec30d5c678593363e876e3
Andres Rosales <andresrosalespinzon@gmail.com>
2021-02-07 12:34:46 +0100

    done with styling, imported forms and started with adding it to html

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-item-form',
   templateUrl: './add-item-form.component.html',
-  styleUrls: ['./add-item-form.component.scss']
+  styleUrls: ['./add-item-form.component.scss'],
 })
 export class AddItemFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  onSubmit(form: NgForm) {}
 }
